09c12f175665de5e60a0ce3fae157e1d675eaba3
Ayush Keshari <119078836+ayushthebeast@users.noreply.github.com>
2024-06-11 00:01:38 +0530

    Add files via upload

---
+++ lecture5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"input integer a,b,c and d"<<endl;
+
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+    cout<<(a>b && c>d);
+    
+}
