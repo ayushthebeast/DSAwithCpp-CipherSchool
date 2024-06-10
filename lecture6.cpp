b535cff985569072276b1771ce0864f07ddd429c
Ayush Keshari <119078836+ayushthebeast@users.noreply.github.com>
2024-06-11 00:25:05 +0530

    Add files via upload

---
+++ lecture6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	
+	switch(a){
+		case 1:
+			cout<<"a is 1";
+			break;
+		case 2:
+			cout<<"a is 2";
+			break;
+		case 3:
+		    cout<<"a is 3";
+			break;
+		default:
+		 	cout<<"this is default";
+			 break;	
+	}
+}
