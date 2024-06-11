2bcc28f6fe99dc38cfb24a114db5404b1982f7fb
Ayush Keshari <119078836+ayushthebeast@users.noreply.github.com>
2024-06-11 23:28:44 +0530

    Add files via upload

---
+++ lecture8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[5];
+	int i;
+	cout<<"give 6 numbers";
+	for(i=0;i<=5;i++){
+		cin>>a[i];
+	}
+	
+	cout<<"your given numbers are";
+	for(i=0;i<=5;i++){
+		cout<<a[i];
+	}
+	
+}
