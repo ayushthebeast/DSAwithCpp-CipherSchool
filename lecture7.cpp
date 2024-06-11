ce5075174a4d42c0978793d09e4a886cd062272a
Ayush Keshari <119078836+ayushthebeast@users.noreply.github.com>
2024-06-11 13:50:19 +0530

    Add files via upload

---
+++ lecture7.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e;
+	cout<<"give 5 integer as input: ";
+	cin>>a>>b>>c>>d>>e;
+	cout<<"the integer in reverse order are: "<<e<<d<<c<<b<<a;
+//	return 0;
+}
