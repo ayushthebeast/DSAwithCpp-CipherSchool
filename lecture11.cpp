aa2fd889b27f12eee7a15a6c9b54c9c88a9e6656
Ayush Keshari <119078836+ayushthebeast@users.noreply.github.com>
2024-06-12 16:37:52 +0530

    Add files via upload

---
+++ lecture11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int i;
+int input;
+for(i=1;i<101;i++){
+	cin>>input;
+	if(input==65){
+		cout<<"congrats, you have guessed it right";
+		break;
+	}
+}
+}
