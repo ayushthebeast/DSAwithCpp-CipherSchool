bb974cda4063f385ed5cecd7d4bd2cd3c6f7f83b
Ayush Keshari <119078836+ayushthebeast@users.noreply.github.com>
2024-06-15 22:44:10 +0530

    Add files via upload

---
+++ lecture20.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	long long int a;
+	int b;
+	cout<<"size of int is: "<<sizeof(b)<<endl<<"size of long long int: "<<sizeof(a)<<endl;
+	cout<<"address of b is: "<<&b<<endl<<"address of a is: "<<&a;
+
+}
